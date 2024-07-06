ed70a66ba0977b65ad16a8c627c98773f7d22101
César Daniel Rodrigues <97107669+Czar1347@users.noreply.github.com>
2024-07-06 14:52:16 -0300

    Métodos estáticos e promessas

--- js/favorites.js
+++ js/favorites.js
@@ -1,13 +1,25 @@
+export class Github {
+  static search(userName) {
+    const url = `https://api.github.com/users/${userName}`
+
+    return fetch(url)
+      .then((data) => data.json())
+      .then(({ login, name, public_repos, followers }) => ({ login, name, public_repos, followers }))
+  }
+}
+
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
     this.load()
+
+    Github.search("maykbrito").then((user) => console.log(user))
   }
   load() {
-    this.entries = JSON.parse(localStorage.getItem('@github-favorites')) || []
+    this.entries = JSON.parse(localStorage.getItem("@github-favorites")) || []
   }
-  delete(user){
-    const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+  delete(user) {
+    const filteredEntries = this.entries.filter((entry) => entry.login !== user.login)
 
     this.entries = filteredEntries
     this.update()
@@ -24,25 +36,24 @@ export class FavoritesView extends Favorites {
   update() {
     this.removeAllTr()
 
-    this.entries.forEach( userInfo =>{
+    this.entries.forEach((userInfo) => {
       const row = this.createRow()
-      row.querySelector('td img').src = `https://github.com/${userInfo.login}.png`
-      row.querySelector('td img').alt = `Imagem de ${userInfo.name}`
-      row.querySelector('td a').href = `https://github.com/${userInfo.login}`
-      row.querySelector('td a p').textContent = `${userInfo.name}`
-      row.querySelector('td.repositories').textContent = `${userInfo.publicRepositorys}`
-      row.querySelector('td.followers').textContent = `${userInfo.followers}`
+      row.querySelector("td img").src = `https://github.com/${userInfo.login}.png`
+      row.querySelector("td img").alt = `Imagem de ${userInfo.name}`
+      row.querySelector("td a").href = `https://github.com/${userInfo.login}`
+      row.querySelector("td a p").textContent = `${userInfo.name}`
+      row.querySelector("td.repositories").textContent = `${userInfo.publicRepositorys}`
+      row.querySelector("td.followers").textContent = `${userInfo.followers}`
 
-      row.querySelector('.remove').onclick = ()=>{
-        const isOK = confirm('Deseja mesmo excluir esse favorites?')
-        if(isOK){
+      row.querySelector(".remove").onclick = () => {
+        const isOK = confirm("Deseja mesmo excluir esse favorites?")
+        if (isOK) {
           this.delete(userInfo)
         }
       }
 
       this.tbody.append(row)
     })
-    
   }
   createRow() {
     const tr = document.createElement("tr")
@@ -64,9 +75,6 @@ export class FavoritesView extends Favorites {
     return tr
   }
   removeAllTr() {
-    
-
-
     this.tbody.querySelectorAll("tr").forEach((tr) => {
       tr.remove()
     })
